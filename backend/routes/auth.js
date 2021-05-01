30d83430377500a31350ac3f70e004f69192a0b7
David Křístek <david.kristek05@gmail.com>
2021-05-01 10:15:41 +0200

    del

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const GoogleAuthController = require("../controllers/googleAuthController");
-const checkAuth = require("../lib/chectAuth");
-
-const googleAuth = new GoogleAuthController();
-router.post("/googlelogin", googleAuth.login);
-router.get("/is_logged", checkAuth, googleAuth.isLogged);
-
-module.exports = router;
